b555cb621d03858a70a045df4da35197656e7a91
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-20 08:43:22 -0400

    Initial Commit

---
+++ src/main/java/dtgp/spring/mscbrewery/MscBreweryApplication.java
@@ -0,0 +1,13 @@
+package dtgp.spring.mscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dtgp/spring/mscbrewery/MscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package dtgp.spring.mscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
